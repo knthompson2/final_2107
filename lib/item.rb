629ede914dd1a0647ea14f8eafab07370259f878
Kelsey Thompson <kelsthompson2@gmail.com>
2021-08-11 11:38:33 -0400

    Complete iteration 2

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,22 @@
 class Item
-  attr_reader :name
+  attr_reader :name, :bids
 
   def initialize(name)
     @name = name
+    @bids = {}
+  end
+
+  def add_bid(attendee, amount)
+    @bids[attendee] = amount
+  end
+
+  def current_high_bid
+    if @bids != {}
+      @bids.max_by do |attendee, amount|
+        amount
+      end.last
+    else
+      0
+    end
   end
 end
